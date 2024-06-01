a4095574e5de91b282ebd6f78380f7debb5e2d7c
taetris <triptis1235@gmail.com>
2024-06-01 20:55:31 +0545

    functions refactored and comments added

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-const items_pending = document.getElementById("items_pending");
-const items_completed = document.getElementById("items_completed");
+const itemsPending = document.getElementById("items_pending");
+const itemsCompleted = document.getElementById("items_completed");
 
 const input = document.getElementById("input");
-const checkbox = document.getElementById("Checkbox");
 
+// Event listener for adding items on "Enter" key press
 input.addEventListener("keyup", (e) =>{
-    if(e.key === "Enter"){
-        addItemToPending(input.value);
+    if(e.key === "Enter" && input.value.trim() !== "") {
+        addItemToPending(input.value.trim());
         input.value = "";
     }
 })
 
-items_pending.addEventListener("change", (e) => {
-    if (e.target.type === "checkbox") {
-        if (e.target.checked) {
-            const checkboxId = e.target.id;
-            addItemToCompleted(checkboxId);
-            removeItemFromPending(checkboxId);
-            
-        }
+// Event listener for moving items between lists when checkbox is checked
+itemsPending.addEventListener("change", (e) => {
+    if (e.target.type === "checkbox" && e.target.checked) {
+        const checkboxId = e.target.id;
+        const id = checkboxId.split("-")[1];
+        const itemValue = document.getElementById(`item-${id}`).innerHTML;
+        addItemToCompleted(itemValue);
+        removeItemFromPending(id);
     }
 });
 
-const addItemToCompleted = (checkboxId) => {
-    let id = checkboxId.split("-")[1];
-    itemValue = document.getElementById(`item-${id}`).innerHTML;
-    items_completed.innerHTML += "<li>"+itemValue + "</li>"+ "<br>";
-}
-const removeItemFromPending = (checkboxId) => {
-    let id = checkboxId.split("-")[1];
+// Function to add item to completed list
+const addItemToCompleted = (itemValue) => {
+    itemsCompleted.innerHTML += `<li>${itemValue}</li><br>`;
+};
+
+// Function to remove item from pending list
+const removeItemFromPending = (id) => {
     document.getElementById(`item-${id}`).remove();
     document.getElementById(`checkbox-${id}`).remove();
-}
+};
 
 
 const createID = () => `${Date.now()}`;
 const createCheckBox = (itemID) =>{
-    let checkbox = `<input type="checkbox" id="checkbox-${itemID}"></input>`;
-    return checkbox;
+    return `<input type="checkbox" id="checkbox-${itemID}"></input>`;
+
 }
 const addItemToPending = (item) => {
     let id = createID();
     let checkBox = createCheckBox(id);
-    console.log(id);
-    console.log(checkBox);
-    itemValue = `<li id="item-${id}">`+ item + "</li>" ;
-    items_pending.innerHTML += itemValue + checkBox + "<br>";
+
+    const listItem = `<li id="item-${id}">${item} ${checkBox}</li>`;
+    itemsPending.insertAdjacentHTML("beforeend", listItem);
 }
 
